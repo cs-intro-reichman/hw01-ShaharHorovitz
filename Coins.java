c8f49d51c4449f6b10e8381f66b734552b570bad
ShaharHorovitz <154262390+ShaharHorovitz@users.noreply.github.com>
2023-12-20 08:41:32 +0200

    Update Coins.java

--- Coins.java
+++ Coins.java
@@ -1,11 +1,11 @@
 public class Coins{
-public static void main(String[] args){
-//gets the number from the user
-int coins = Integer.parseInt(args[0]);
-//checks how many quarters by dividing the number in 25
-int quarters = coins /25;
-//checks how many cents by checking the remainder of division
-int cents = coins % 25;
-System.out.println("use " + quarters + " quarters and " + cents + " cents");
-}
-}
+  public static void main(String[] args){
+  //gets the number from the user
+  int coins = Integer.parseInt(args[0]);
+  //checks how many quarters by dividing the number in 25
+  int quarters = coins/25;
+  //checks how many cents by checking the remainder of division
+  int cents = coins % 25;
+  System.out.println("use " + quarters + " quarters and " + cents + " cents");
+  }
+  }

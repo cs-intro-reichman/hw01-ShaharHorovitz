5a5e5a603273ffc65f89c5927b16dbe48f958cf9
ShaharHorovitz <154262390+ShaharHorovitz@users.noreply.github.com>
2023-12-21 08:28:24 +0200

    Update GenThree.java

--- GenThree.java
+++ GenThree.java
@@ -4,20 +4,23 @@
  */
 public class GenThree {
 	public static void main(String[] args) {
-		//gets the minimum and maximum range to generate numbers from the user
+	//gets the minimum and maximum range to generate numbers from the user
 	int minNum = Integer.parseInt(args[0]);
 	int maxNum = Integer.parseInt(args[1]);
 	double r = Math.random();
 	//checks number's range 
 	int range = (maxNum - minNum);
-	//genergates the first random number
+	//genergates the first random number:
+	//it multiplys the range in the random number between (0-1), then adds the minimum number in order to get a number in the given range (min- max)
 	int minRand = (int) (range * r) +minNum;
 	System.out.println(minRand);
-	int n;
+	//defines an int to store the next random number
+	int n = 0;
  	for (int i = 0; i < 2; i++){
 		r = Math.random();
 		n = (int)(range *r) +minNum;
 		System.out.println(n);
+		//checks if the number generated is lower then the first number,if yes- updates the minimum number
 		if (n < minRand){
 		minRand = n;}
 	}
